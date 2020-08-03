ab01f1ef002c5dad816ab368a26054a7d01b3e75
Alexei <61819719+Alexes012@users.noreply.github.com>
2020-08-03 14:41:45 +0300

    Updates

---
+++ precache-manifest.14f52f91a8779e43c28223c8aeed37c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b2eec1aad22915cbda79978ffa1e994",
+    "url": "/todolist-ready/index.html"
+  },
+  {
+    "revision": "3bae8bdbef5b0d1e1a6a",
+    "url": "/todolist-ready/static/css/main.df969bfc.chunk.css"
+  },
+  {
+    "revision": "2bc083c0024e03f43ed8",
+    "url": "/todolist-ready/static/js/2.5b645b2d.chunk.js"
+  },
+  {
+    "revision": "3bae8bdbef5b0d1e1a6a",
+    "url": "/todolist-ready/static/js/main.cc7bda6c.chunk.js"
+  },
+  {
+    "revision": "222a0268452f8afb88bf",
+    "url": "/todolist-ready/static/js/runtime~main.be1c8eb8.js"
+  }
+]);
